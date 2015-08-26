e8c1fbf3e221fc5158cf01167dc602af5daa388e
Raphael <gemini88mill@gmail.com>
2015-08-26 15:23:14 -0400

    added school_projects and Sudokode

--- Physics/main.cpp
+++ Physics/main.cpp
@@ -5,18 +5,39 @@
 
 float find_force(int i);
 
+void impact();
+
+void motion();
+
 int main(int argc, char *argv[]) {
     int mass = 5;
 
-   // printf("%d", ACCELERATION);
-
-    //int forca = mass * ACCELERATION;
-
     float result = find_force(mass);
-
     printf("Force = %0.2f m/s", result);
-   // printf("Forca = %i\n", forca);
+
+    //impact equations
+    impact();
+
+    //one dimensional rocket problems
+    motion();
+
     return 0;
+}
+
+void motion() {
+    //
+}
+
+void impact() {
+    //mass of object to be hit...
+    int mass_wall = 5;
+    int mass_object = 10;
+
+    //Ft = m*deltaV
+
+    //find force of object
+    /**/
+
 }
 
 float find_force(int i) {
