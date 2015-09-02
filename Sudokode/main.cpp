40c005f55f6675f98e8fb0de14973dae09cfb116
Raphael <gemini88mill@gmail.com>
2015-09-01 23:35:21 -0400

    almost done with in file format

--- Sudokode/main.cpp
+++ Sudokode/main.cpp
@@ -1,8 +1,8 @@
-#include <stdlib.h>
+
 #include <stdio.h>
 
 #define SIZE 9
-#define SUBGRID_SIZE 3
+
 
 
 bool check_row_logic(int pInt[]);
@@ -13,9 +13,11 @@ bool column_level(int prepared_row[], int solution[][SIZE], bool row_check);
 
 bool subgrid_level(int prepared_row[], int solution[][SIZE], bool row_check);
 
+unsigned int count(int i);
+
 int main() {
 
-    int solution[SIZE][SIZE] = {{3,5,7,6,4,8,9,1,2},
+    int solution[SIZE][SIZE];/* = {{3,5,7,6,4,8,9,1,2},
                                 {2,1,6,5,3,9,7,4,8},
                                 {9,4,8,7,1,2,5,3,6},
                                 {5,2,1,4,8,6,3,9,7},
@@ -23,34 +25,88 @@ int main() {
                                 {7,8,9,3,2,5,1,6,4},
                                 {6,3,2,9,7,4,8,5,1},
                                 {1,7,4,8,5,3,6,2,9},
-                                {8,9,5,2,6,1,4,7,3}};
+                                {8,9,5,2,6,1,4,7,3}};*/
+
+    //int test[SIZE];
+
+    int number_of_solutions = 0;
+    scanf("%i", &number_of_solutions);
+
+    int i, j, k;
+    int value = 0;
+    int explode[SIZE];
+    int count = 0;
+
+    for(k = 0; k < number_of_solutions; k++) {
+        for (i = 0; i < SIZE; i++) {
+            scanf("%d", &value);
+
+            while ( value > 0 )
+            {
+                explode[count] = value % 10;
+                value = value / 10;
+                count++;
+            }
+            for(j = 0; j < SIZE; j++) {
+                for (int i = SIZE - 1; i >= 0; i--) {
+                    solution[0][i] = explode[i];
+                    printf("%d", solution[j][i]); // work on this for file in :) 
+                }
+            }
+        }
+    }
+
+    while ( value > 0 )
+    {
+        explode[count] = value % 10;
+        value = value / 10;
+        count++;
+    }
+    for(j = 0; j < SIZE; j++) {
+        for (int i = SIZE - 1; i >= 0; i--) {
+            solution[0][i] = explode[i];
+            printf("%d", solution[0][i]);
+        }
+    }
 
 
     int prepared_row[SIZE];
     bool row_check = false; //boolean value to check store value of test.
     bool final_check[3] = {false, false, false};
-    int i, j, k, l, m;
+
 
 
     //------------------------ row level --------------------------------
-    final_check[0] = row_level(prepared_row, solution, row_check);
+    //final_check[0] = row_level(prepared_row, solution, row_check);
     //-------------------------------------------------------------------
 
     //--------------------- Column level -------------------------------
     printf("\n\n");
-    final_check[1] = column_level(prepared_row, solution, row_check);
+    //final_check[1] = column_level(prepared_row, solution, row_check);
     //------------------------------------------------------------------
 
     //----------------------subgrid level-------------------------------
     printf("\n\n");
-    final_check[2] = subgrid_level(prepared_row, solution, row_check);
+    //final_check[2] = subgrid_level(prepared_row, solution, row_check);
     //------------------------------------------------------------------
 
     printf("%s", final_check[2] ? "true\n" : "false\n");
     printf("%s", final_check[1] ? "true\n" : "false\n");
     printf("%s", final_check[0] ? "true\n" : "false\n");
 
+    if(final_check[0] == true && final_check[1] == true && final_check[2] == true){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+
     return 0;
+}
+
+unsigned int count(int i) {
+    unsigned int ret=1;
+    while (i/=10) ret++;
+    return ret;
 }
 
 bool subgrid_level(int prepared_row[], int solution[][SIZE], bool row_check) {
