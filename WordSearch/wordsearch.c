d6e689d47ca490cb8fa71d050387c8667a008517
Raphael <gemini88mill@gmail.com>
2015-09-10 23:02:36 -0400

    still trying to figure out dictionary

--- WordSearch/wordsearch.c
+++ WordSearch/wordsearch.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define TXT_FILE "/home/raphael/ClionProjects/WordSearch/dictionary.txt"
+#define CHUNK 1024
 
 int dictionary();
 
@@ -11,15 +12,15 @@ int main() {
     //printf("hello, world");
 
     /*
-     * Word Search - allows the user to input test cases for a word search and outputs solutions to the terminal.
+     * Word Search - allows the user to input test cases for a word_str search and outputs solutions to the terminal.
      *  Patent Pending TM 2015
      *
-     *  Uses dictionary file that has every word in the english dictionary, gathered from a pre-web2.0 website
+     *  Uses dictionary file that has every word_str in the english dictionary, gathered from a pre-web2.0 website
      *  Source: http://www.angelfire.com/extreme4/safer_sephiroth/EVERY_WORD_EVER.htm
      *
      *  first input line: int - for number of test cases
      *  second input line: int x 2 - for number of rows and columns
-     *  next input lines: chars(rows)(columns) - for the actual letters for the word search
+     *  next input lines: chars(rows)(columns) - for the actual letters for the word_str search
      *  rinse and repeat
      *
      *  ...*/
@@ -37,41 +38,35 @@ int dictionary() {
      * Dictionary function:
      * initializes the dictionary to be used in the program.
      * */
-    FILE *txt_file = fopen ( TXT_FILE, "r");
-    char *word = malloc(sizeof(char));
-    char **hold = malloc(sizeof(char)*sizeof(char));
-    int index = 0;
 
-    //char ch = getc (txt_file);
+    //------------------variable declaration-----------------------------
+    FILE *txt_file;                                                 //file
 
-        if (word == NULL) {
-            perror("Error opening file");
-            return (-1);
-        }
-    while(txt_file != NULL) {
-        if (fgets(word, 100, txt_file) != NULL) {
-            puts(word);
+    char *word_str = malloc(sizeof(char *) * sizeof(char));
+    char tester[1024];
+    int index = 0, rows = 0 ;
+    int buff_size = 64;
+    int txt_file_length;
+    size_t nread;
+    //-------------------------------------------------------------------
 
-        }
-        hold[index] = word;
-        index++;
+
+
+    txt_file = fopen(TXT_FILE, "r");
+    if (txt_file == NULL){
+        return 1;
     }
 
-
-
-    fclose (txt_file);
-    free(word);
-
-    //---prints out file contents for debug------------------------
-
-
-    //-------------------------------------------------------------
-
-
-    fill_array_dictionary();
-
-    return 0;
-
+    if (txt_file) {
+        while ((nread = fread(word_str, 1, sizeof buff_size, txt_file)) > 0) {
+            //fwrite(word_str, 1, nread, stdout);
+            fgets(word_str, sizeof nread, txt_file);
+        }
+        if (ferror(txt_file)) {
+            /* deal with error */
+        }
+        fclose(txt_file);
+    }
 }
 
 void fill_array_dictionary() {
