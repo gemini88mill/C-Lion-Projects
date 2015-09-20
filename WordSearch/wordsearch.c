345c3ce9279d214cc7867a7134bc1c552e815c85
Raphael <gemini88mill@gmail.com>
2015-09-20 00:08:09 -0400

    horizontal search working, does not shift

--- WordSearch/wordsearch.c
+++ WordSearch/wordsearch.c
@@ -13,6 +13,8 @@
 
 #define TXT_FILE "/home/raphael/ClionProjects/WordSearch/dictionary.txt"
 #define TEST_FILE "/home/raphael/ClionProjects/WordSearch/hello.txt"
+#define TRUE 1
+#define FALSE 0
 
 typedef struct{
     size_t used;
@@ -20,7 +22,7 @@ typedef struct{
     char **words;
 } Entries;
 
-void initEntries(Entries *a, size_t initial_size);
+void init_Entries(Entries *a, size_t initial_size);
 void insertEntries(Entries *a, char *element);
 void freeEntries(Entries *a);
 char *strdup (const char *s);
@@ -31,11 +33,13 @@ char *get_puzzle_string();
 
 int binary_search(char **pString, char *search_ptr, int low, int high, size_t length);
 
+int find_horizontal(char **dictionary, char **grid, unsigned int rows, unsigned int colomns, size_t i);
+
 int main() {
 
     //dictionary level variables ------------------------------------------------
     FILE *txt_file = fopen(TXT_FILE, "r");
-    Entries word;
+    Entries dictionary;
     int j = 0;
     char buff[32][BUFSIZ];
     char *p;
@@ -49,33 +53,35 @@ int main() {
     char **puzzle_block;
     char *puzzle_values;
 
-    int k, l;
+
     //---------------------------------------------------------------------------
 
     //binary search variables ---------------------------------------------------
     char search_string[32]; //testing search string.
     char *search_ptr = search_string; //ptr to binary search string.
 
+    int result;
+    //---------------------------------------------------------------------------
 
+    //grid values----------------------------------------------------------------
 
-    int i = 0,result, x = 0;
     //---------------------------------------------------------------------------
 
 
-    initEntries(&word, 5); //initialize char array for dictionary
+    init_Entries(&dictionary, 5); //initialize char array for dictionary
 
     //block dealing with dictionary value allocation-------------------------------------------
     while(j < sizeof(buff) && fgets(buff[j], sizeof(buff[0]), txt_file)) {
         p = strdup(buff);
-        insertEntries(&word, p);
+        insertEntries(&dictionary, p);
     }
     fclose(txt_file);
     //-----------------------------------------------------------------------------------------
 
     //check if values are there (debug)------------------------------
-    printf("element 0:%s\n", word.words[1]);
-    printf("element 1:%s\n", word.words[34534]);
-    printf("element 2:%s\n", word.words[114877]);
+    //printf("element 0:%s\n", dictionary.words[1]);
+    //printf("element 1:%s\n", dictionary.words[34534]);
+    //printf("element 2:%s\n", dictionary.words[114877]);
     //---------------------------------------------------------------
 
     //accept in file arguements----
@@ -87,39 +93,93 @@ int main() {
     //binary search ------------------------------------------------
     scanf("%s", search_ptr);
 
-    if((result = binary_search(word.words, search_ptr, 0, (int) word.used - 1, sizeof(search_ptr))) != 0){
-        printf("%d", result);
+    if((result = binary_search(dictionary.words, search_ptr, 0, (int) dictionary.used - 1, sizeof(search_ptr))) != 0){
+        //printf("%d", result);
 
-       printf("found %s at position %d", search_ptr, result);
+       printf("found %s at position %d\n", search_ptr, result);
     } else{
         printf("%d", result);
         printf("%s not found", search_ptr);
     }
     //--------------------------------------------------------------
 
+    //grid level----------------------------------------------------
+    Entries grid;
+    char grid_buff[32];
+    char *pp;
+    int k;
+
+    init_Entries(&grid, 5);
+
+    for(k = 0; k < rows; k++){
+        scanf("%s", grid_buff);
+        pp = strdup(grid_buff);
+        insertEntries(&grid, pp);
+        //printf("%s\n", grid.words[k]);
+    }
+
+    int check;
+
+    //function find_horizontally
+    /*start position*/
+    check = find_horizontal(dictionary.words, grid.words, rows, colomns, dictionary.used - 1);
+    //--------------------------------------------------------------
 
     return 0;
 }
 
+int find_horizontal(char **dictionary, char **grid, unsigned int rows, unsigned int colomns, size_t i) {
+    //finds all horizontal values in grid.
+    char grid_section[32];
+    char section_test[32];
+    size_t t;
+    int check;
+    size_t high = i;
+
+    memset(section_test, '\0', sizeof(section_test));
+    strcpy(grid_section, *grid);
+
+    for(t = 2; t < sizeof(grid_section); t++) {
+        strncpy(section_test, grid_section, t);
+        //printf("%s\n", section_test);
+        check = binary_search(dictionary, section_test, 0, (int) (high - 1), sizeof(section_test));
+        if(check != 0){
+            printf("found word\n");
+        } else {
+            printf("did not find word\n");
+        }
+    }
+
+
+    return FALSE;
+}
+
 int binary_search(char **pString, char *search_ptr, int low, int high, size_t length) {
 
+    /*local store chars*/
     char key_string[16];
     char dictionary_string[16];
 
-
-
+    //test for end of recurse loop
     if (high < low) {
         return 0;
     }
+
     int mid = (low + high) / 2;
     int result;
 
+    //copy values into locals
     strcpy(dictionary_string, pString[mid]);
     strcpy(key_string, search_ptr);
 
+    //remove \r\n from dictionary_string
     dictionary_string[strcspn(dictionary_string, "\r\n")] = 0;
 
-    printf("%s\n", dictionary_string);
+    //print (for debug)
+    //printf("%s\n", dictionary_string);
+
+
+    //recursed binary search ------------------------------------------------
     result = strncmp(dictionary_string, key_string, length);
     if (result > 0) {
         return binary_search(pString, key_string, low, mid - 1, length);
@@ -128,8 +188,8 @@ int binary_search(char **pString, char *search_ptr, int low, int high, size_t le
     } else if (result == 0)  {
         return mid;
     }
-
-
+    //-----------------------------------------------------------------------
+    return 0; //will never get here but compiler is yelling at me and I don't like it to yell :)
 }
 
 
@@ -148,7 +208,7 @@ char *strdup (const char *s) {
 }
 
 //------------functions pertaining to dictionary mem block-------------------------------------------
-void initEntries(Entries *a, size_t initial_size){
+void init_Entries(Entries *a, size_t initial_size){
     a->words = malloc(initial_size * sizeof(char*));
     a->used = 0;
     a->size = initial_size;
