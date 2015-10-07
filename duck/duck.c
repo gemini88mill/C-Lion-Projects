9bdf782b222c527ed945af0452da0e87d1cfc1a6
Raphael <gemini88mill@gmail.com>
2015-10-07 16:37:15 -0400

    game logic correct and tested

--- duck/duck.c
+++ duck/duck.c
@@ -48,7 +48,7 @@
 
 void gather_input(int *games, int *friends, char **friend_names, int *rounds, int *ducks);
 
-void manipulate_input();
+void manipulate_input(int *friends_length, int *ducks, char **friend_names, int i);
 
 void output();
 
@@ -62,6 +62,10 @@ char *strdup (const char *s);
 void get_name(char **pString, int counter, char buff[]);
 
 int get_int_arr(int *pInt, int limit);
+
+char **game_logic(char **friends_list, int *ducks, int list_length, int i);
+
+char **remove_from_list(char **previous_list, char *player, int pos, int list_length);
 
 int main() {
     //----------------------------------------------
@@ -77,15 +81,17 @@ int main() {
     //----------------------------------------------
     gather_input(&no_of_games, &no_of_friends, friend_names, &rounds, &ducks);
 
-    printf("%d\n", no_of_games);
-    printf("%d\n", no_of_friends);
+//    printf("%d\n", no_of_games);
+//    printf("%d\n", no_of_friends);
+//
+//    for(i = 0; i < no_of_friends; i++)
+//        printf("%s", friend_names[i]);
+//
+//    printf("%d\n", rounds);
+//    printf("%d\n", ducks);
 
-    for(i = 0; i < no_of_friends; i++)
-        printf("%s", friend_names[i]);
 
-    printf("%d\n", rounds);
-    printf("%d\n", ducks);
-    manipulate_input();
+    manipulate_input(&no_of_friends, &ducks, friend_names, rounds);
     output();
     return 0;
 }
@@ -94,8 +100,88 @@ void output() {
 
 }
 
-void manipulate_input() {
+void manipulate_input(int *friends_length, int *ducks, char **friend_names, int rounds) {
+    /*  manipulate input() - in this scenario the idea is that jimmy will have a number of friends once he starts
+     *  playing the game, and then, depending on how many rounds the game is played, loses friends systematically,
+     *
+     *  code flow - jimmy will start at pos 0
+     *              jimmy will then move through the list for (duck) moves until he has reached his target
+     *              once jimmy reaches his target, that person is out of the game. the game is played until
+     *              a. there are no more friends left or
+     *              b. the game is over
+     *
+     *  functions in logic:
+     *      game_logic() - the primary function of the game.
+     *          game logic is set up in two parts:
+     *              first if number of rounds is less then number of friends
+     *                if friends_length > ducks
+     *              -if this is the case then we can kick out friend_names[ducks + 1]
+     *              -delete friend from game
+     *              -next round
+     *
+     *          if friends_length < ducks
+     *              -person kicked = (friends_length % ducks) + 1
+     *              -next round
+     *     Note: friends_names should be copied to another function as to not ruin the original data just in case...*/
+    int no_of_friends = *friends_length;
+    char **friends_list = malloc(sizeof(char) * no_of_friends * 20);
+    char **winners = malloc(sizeof(char) * no_of_friends * 20);
 
+    int i;
+
+    for (i = 0; i < no_of_friends; i++){
+        friends_list[i] = friend_names[i];
+    }
+
+    game_logic(friends_list, ducks, no_of_friends, rounds);
+
+}
+
+char **game_logic(char **friends_list, int *ducks, int list_length, int rounds) {
+    int pos = 0;
+
+    printf("ducks: %d\t", *ducks);
+    printf("listlength: %d", list_length);
+
+    while (rounds > 0) {
+        if (list_length > *ducks) {
+            pos = *ducks;
+            printf("friend: %d, %s", pos + 1, friends_list[pos]);
+            //remove and restructure
+        } else if (list_length < *ducks) {
+            pos = *ducks % list_length;
+            printf("friend: %d, %s", pos + 1, friends_list[pos]);
+            //remove and restructure
+        }
+        //friends_list = remove_from_list(friends_list, friends_list[pos], pos, list_length);
+        rounds--;
+    }
+
+    return NULL;
+}
+
+char **remove_from_list(char **previous_list, char *player, int pos, int list_length) {
+    char **new_list = malloc(sizeof(char) * 20 * list_length);
+    int i = 0;
+    if(pos >= list_length + 1){
+        perror("Error: out of bounds");
+    } else {
+        while (i != pos -1){
+            i++;
+        }
+        while(i < list_length){
+            previous_list[i] = previous_list[i + 1];
+            i++;
+        }
+        list_length--;
+    }
+
+    for(i = 0; i < list_length; i++){
+        new_list[i] = previous_list[i];
+        printf("%s\n", previous_list[i]);
+        printf("%s\n", new_list[i]);
+    }
+    return new_list;
 }
 
 void gather_input(int *games, int *friends, char **friend_names, int *rounds, int *ducks) {
@@ -105,6 +191,7 @@ void gather_input(int *games, int *friends, char **friend_names, int *rounds, in
     char buff[BUFSIZ];
     char *temp;
     int read_check;
+    int ch;
 
     //scans in the number of games
     /*  gets the amount of games for jimmy the sadist to play. Put into a for loop so we can gather the information
@@ -112,12 +199,16 @@ void gather_input(int *games, int *friends, char **friend_names, int *rounds, in
     get_ints(&games, 1);
     get_ints(&friends, 1); //scans in the number of friends.
 
-    for (counter = 0; counter < *games; counter++){
+    printf("%d", *games);
+    printf("%d\n", *friends);
+
+    //clearing input buffer... took me forever to figure this out...
+    while ((ch = getchar()) != '\n' && ch != EOF);
+
+    for (counter = 0; counter < *friends; counter++){
         /*  for loop for the input data from the game. everything points outside for the loop because we need to bring
             the data to other parts of the program*/
         get_name(friend_names, counter, buff);
-
-
     }
     //get_int_arr(rd_ptr, 1);
 
@@ -153,15 +244,15 @@ char *strdup (const char *s) {
     return d;                            // Return new memory
 }
 
-
-
 //function to pull ints from data
 int get_ints(int **pInt, int limit) {
-    int i = 0;
+
+    int i = 1;
     int result;
-    while (i < limit && (result = scanf("%d *[^\n]", *pInt) == 1))
+    while (i <= limit && (result = scanf("%d*[^\n]", *pInt) == 1))
         i++;
 
     return result;
+    return 0;
 }
 
