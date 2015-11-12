9e24a0ebed26deca4addff1d36c7b2b503b42a64
Raphael <gemini88mill@gmail.com>
2015-11-11 21:26:53 -0500

    minor bug with sample input

--- cake/cake.c
+++ cake/cake.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int find_perimeter(int area);
+double find_perimeter(double area);
 
 int main() {
-    printf("hello world this is cake");
+    //printf("hello world this is cake");
+    int input_cases, i;
+    double input;
+    double result;
 
-    find_perimeter(169);
+    scanf("%d", &input_cases);
+
+    for(i = 0; i < input_cases; i++) {
+        scanf("%lf", &input);
+        result = find_perimeter(input);
+        printf("%f\n", result);
+    }
 
     return 0;
 }
 
-int find_perimeter(int area){
-    int perim = 0;
+double find_perimeter(double area){
+    double perim = 0;
     double side = 0;
 
     /*logic: given AREA, find minimum perimeter
@@ -23,7 +32,8 @@ int find_perimeter(int area){
      *  2(side + 2(side) = Perimeter*/
 
     side = sqrt(area);
-    printf("side: %0.0f", side);
+    //printf("side: %0.0f", side);
+    perim = (2*side + 2*side);
 
     return perim;
 }
