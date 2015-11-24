f80c5146274afe1acccc187f58020ee14b21b9b3
Raphael <gemini88mill@gmail.com>
2015-11-24 17:58:11 -0500

    fleshing out convert to GATTACA

--- genetics/genetics.c
+++ genetics/genetics.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define A 0
 #define C 1
 #define G 2
 #define T 3
+#define BASE 4
 
 
 void convert_to_base_ten(char buff[15]);
@@ -32,7 +34,24 @@ int main() {
 
 void convert_to_gen_code(char buff[15]) {
     //converts string received into GATTACA genetic code
-    
+    int i, index = 0; //looper
+    int base_four_num[16] = {0};  //converted number
+
+    //convert to int
+    int base_10_num = atoi(buff); //number to convert
+    //convert to base 4
+
+    while (base_10_num != 0) {
+        base_four_num[index] = base_10_num % BASE;
+        base_10_num = base_10_num / 4;
+        index++;
+    }
+
+    for(i = index; i >= 0; i--){
+        printf("%d ", base_four_num[i]);
+
+    }
+
 }
 
 void convert_to_base_ten(char buff[15]) {
