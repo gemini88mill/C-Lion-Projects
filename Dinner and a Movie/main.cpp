db46d8d187f6830fdf816b4a579cfcd92c7cce7f
Raphael <gemini88mill@gmail.com>
2015-12-01 17:00:25 -0500

    Dinner and a movie completed with test cases verified

--- Dinner and a Movie/main.cpp
+++ Dinner and a Movie/main.cpp
@@ -1,8 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TICKET_COST 10
+#define PIZZA_COST 2
 
 int main() {
-    printf("hello world");
+    int testcases = 0, i, tickets = 0, pizza = 0, total_cost = 0;
+
+    scanf("%d", &testcases); //collects the number of test cases
+
+    for(i = 0; i < testcases; i++){
+        scanf("%d %d", &tickets, &pizza);
+
+        if(tickets < 10){
+            total_cost = (tickets * TICKET_COST) + (pizza * PIZZA_COST);
+
+        }else{
+            total_cost = (tickets * TICKET_COST) + pizza;
+        }
+        printf("%d\n", total_cost);
+    }
     return 0;
 }
