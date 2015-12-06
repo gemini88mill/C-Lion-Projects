ab43435fff3f787625bd2b653b854cd7d4f84f30
Raphael <gemini88mill@gmail.com>
2015-12-06 00:18:30 -0500

    fixed weird things

--- gameofgames/gameofgame.c
+++ gameofgames/gameofgame.c
@@ -7,27 +7,26 @@
 
 struct House_data {
     int house_name;
-
-
     int is_visited;
     int is_open;
-
     int open_time;
     int close_time;
 };
 
 struct Distance_Handler{
-    int distance_from;
-    int distance_to;
+    int *distance_from;
+    int *distance_to;
 };
 
 struct Game_Time_Handler{
     int current_game_time;
+    int number_of_houses;
+    int houses_visited;
 };
 
 
 void logic(struct House_data *house_data, struct Distance_Handler *distance_handler,
-           struct Game_Time_Handler time_handler);
+           struct Game_Time_Handler time_handler, int location);
 
 int check_if_open(int game_time, struct House_data house_data);
 
@@ -52,12 +51,11 @@ int main() {
 
     for(i = 0; i < input_cases; i++){
         scanf("%d", &games_to_complete); //number of games to complete
-
+        game_time.number_of_houses = games_to_complete;
         //allocates memory for number of houses + 1 (for home base)
-        house = malloc(sizeof(house) * games_to_complete + 1);
+        house = malloc(sizeof(struct House_data) * games_to_complete + 1);
         //allocates memory for number of distances to and from houses(including home base)
-        distance_handler = malloc(sizeof(distance_handler) * games_to_complete + 1);
-
+        distance_handler = malloc(sizeof(struct Distance_Handler) * games_to_complete + 1);
 
         int **travel_times = malloc(sizeof(int*) * games_to_complete + 1);
         int n;
@@ -66,18 +64,18 @@ int main() {
         }
         int l, m;
 
-
-        for(j = 0; j < games_to_complete; j++){
+        int gg = games_to_complete;
+        for(j = 0; j < gg; j++){
 
             //collection of travel time.
-            scanf("%d %d", &starting_time, &ending_time);
+            scanf("%d %d", &house[j + 1].open_time, &house[j + 1].close_time);
             //from here we have p(i) and the starting time and ending time for said values. introduce struct
 
             /*sets house variables within the struct, designates the start and end times for each house. sets them to
              * j + 1 because there is no start or end time for home_base house. So start and end times are in house 1
              * ans house 2*/
-            house[j + 1].open_time = starting_time;
-            house[j + 1].close_time = ending_time;
+//            house[j + 1].open_time = starting_time;
+//            house[j + 1].close_time = ending_time;
 
         }
         house[0].open_time = 0;
@@ -87,30 +85,37 @@ int main() {
 //            printf("house[%d] close: %d\n", j, house[j].close_time);
 //        }
 
-        printf("house: %d ", house[1].open_time);
-        printf("house: %d \n", house[1].close_time);
+
         //nested loop to collect travel times...
         for(l = 0; l < games_to_complete + 1; l++){
+            distance_handler[l].distance_to = malloc(sizeof(int) * games_to_complete + 1);
             for(m = 0; m < games_to_complete + 1; m++){
                 scanf("%d", &travel_times[m][l]);
-                distance_handler[l].distance_to = travel_times[m][l];
+                distance_handler[l].distance_to[m] = travel_times[m][l];
                 //printf("\ndis to: %d ", distance_handler[l].distance_to);
             }
-            printf("\n");
+            //printf("\n");
         }
+
         for(l = 0; l < games_to_complete + 1; l++){
+            distance_handler[l].distance_from = malloc(sizeof(int) * games_to_complete + 1);
             for(m = 0; m < games_to_complete + 1; m++){
-                distance_handler[l].distance_from = travel_times[l][m];
+                distance_handler[l].distance_from[l] = travel_times[l][m];
                 //printf("dis from: %d \n", distance_handler[l].distance_from);
             }
         }
-        printf("house: %d ", house[1].open_time);
-        printf("house: %d \n", house[1].close_time);
+
 
         printf("\n");
 
+        int current_location = 0;
+        game_time.houses_visited = 0;
         //todo place logic function here
-        logic(house, distance_handler, game_time);
+        logic(house, distance_handler, game_time, current_location);
+        if(game_time.houses_visited == game_time.number_of_houses)
+            printf("all houses visited");
+
+        printf("\n");
 
     } // end of big loop
     //free(house);
@@ -121,22 +126,38 @@ int main() {
 }
 
 void logic(struct House_data *house_data, struct Distance_Handler *distance_handler,
-           struct Game_Time_Handler time_handler) {
-    //logic...
+           struct Game_Time_Handler time_handler, int location) {
+    if(time_handler.houses_visited == time_handler.number_of_houses)
+        return;
 
-    house_data[1].is_open = check_if_open(6, house_data[1]);
+    //logic...
+    int i;
+
+
+    /*for loop to check houses for opening, if open do more stuff...*/
+    for(i = 0; i < time_handler.number_of_houses + 1; i++) {
+            house_data[i].is_open = check_if_open(distance_handler[location].distance_to[i], house_data[i]);
+        if(house_data[i].is_open == 1){
+            house_data->is_visited = 1; //marks house for visited
+            location = i;
+            time_handler.houses_visited++;
+            logic(house_data, distance_handler, time_handler, location);
+        }
+    }
 
 }
 
 int check_if_open(int game_time, struct House_data house_data) {
     //function to return if particular house is open, if house is open, return 1, if not, return 0
     if (game_time > house_data.open_time && game_time < house_data.close_time){
-        //printf("house is open");
+        printf("house is open\n");
         return 1;
     }
-    //printf("house is closed");
+    printf("house is closed\n");
     return 0;
 }
+
+
 
 //todo logic function: requiring system for keeping track of time, distance, and visiting of houses
 
